f59cb44000e0df0c997bbac7ec8d0de8b612fe7b
Le-TRex <clarembo@gmail.com>
2021-05-10 14:06:17 +0200

    :card_file_box: liste des producteurs accessible

--- controllers/ProducerController.js
+++ controllers/ProducerController.js
@@ -1,5 +1,9 @@
-class ProducerController {
+const Producer = require('../models').Producer; //models => index des models qui charge tous le smodèles puis .ModelSouhaité
 
+class ProducerController {
+  async getAll() {
+    return Producer.findAll();
+  }
 }
 
 module.exports = new ProducerController();
--- routes/producers.js
+++ routes/producers.js
@@ -1,9 +1,11 @@
+const Producer = require('../controllers').ProducerController; //syntaxe cheloue cf models
+
 let express = require('express');
 let router = express.Router();
 
 //grace au router, créer une requête get. Paramètres : url, fonction(requete, reponse, ensuite)
 router.get('/producers', async(req, res, next) => {
-  res.json({hello: "world"});
+  res.json(await Producer.getAll());
 });
 
 module.exports = router;
