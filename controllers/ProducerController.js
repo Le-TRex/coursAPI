22b4cbe94b2aa5e91de9024aa268ab22577a9087
Le-TRex <clarembo@gmail.com>
2021-05-10 14:56:36 +0200

    :card_file_box: créer un producteur

--- controllers/ProducerController.js
+++ controllers/ProducerController.js
@@ -4,6 +4,17 @@ class ProducerController {
   async getAll() {
     return Producer.findAll();
   }
+
+  async getById(id) {
+    return Producer.findByPk(id)
+  }
+
+  async add(firstName, lastName) {
+    return Producer.create({
+      firstName,
+      lastName
+    })
+  }
 }
 
 module.exports = new ProducerController();
--- routes/producers.js
+++ routes/producers.js
@@ -1,11 +1,24 @@
-const Producer = require('../controllers').ProducerController; //syntaxe cheloue cf models
+const ProducerController = require('../controllers').ProducerController; //syntaxe cheloue cf models
 
 let express = require('express');
 let router = express.Router();
 
 //grace au router, créer une requête get. Paramètres : url, fonction(requete, reponse, ensuite)
 router.get('/producers', async(req, res, next) => {
-  res.json(await Producer.getAll());
+  res.json(await ProducerController.getAll());
+});
+
+router.get('/producers/:id', async(req, res, newt) => {
+  const producer = await ProducerController.getById(req.params.id)
+  res.json(producer)
+});
+
+router.post('/producers', async(req, res, next)=>{
+  if(req.body.firstName && req.body.lastName) {
+    const insertedProducer = await  ProducerController.add(req.body.firstName, req.body.lastName);
+    //status = code HTTP + json = la ressource créée
+    res.status(201).json(insertedProducer)
+  }
 });
 
 module.exports = router;
