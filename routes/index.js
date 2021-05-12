aa4c9b39ab2f9fdf4b0223539b9a877f616317c1
Alix <a.boufflers@it-students.fr>
2021-05-12 11:44:53 +0200

    presque :sparkles: begin to setup token authentification

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 const producerRouter = require('./Producers');
 const movieRouter = require('./Movies');
 const genreRouter = require('./Genres');
-
+// var jwt = require('jsonwebtoken');
+// let verify = false
 module.exports = function(app){
-  app.use('/api', [
-    producerRouter,
-    movieRouter,
-    genreRouter,
-  ]);
+  // app.get('/token', function(req, res){
+  //   var token = jwt.sign({username:"superUser"}, 'supersecret');
+  //   res.send(token)
+  // }),
+  // app.get('/api', function(req, res){
+  //   var token = req.query.token;
+  //   jwt.verify(token, 'supersecret', function(err, decoded){
+  //     if(!err){
+  //       verify = true
+  //     } else {
+  //       res.send(err);
+  //     }
+  //   })
+  // })
+
+    app.use('/api', [
+      producerRouter,
+      movieRouter,
+      genreRouter,
+    ])
+
+  
 };
